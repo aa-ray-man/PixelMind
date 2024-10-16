f530b62e841550880105888c10ef41f17381f95d
aryaman <mohindruaryaman@gmail.com>
2024-10-17 00:45:45 +0530

    landing page, database and authentication

---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { Button } from './ui/button'
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { signIn, useSession } from 'next-auth/react';
+import { BiLoaderCircle } from "react-icons/bi";
+
+export default function Header() {
+    const [initialLoading, setInitialLoading] = useState<boolean>(true)
+    const { data: session, status } = useSession();
+    useEffect(() => {
+        if(status !== "loading"){
+            setInitialLoading(false);
+        }
+    },[status, session])
+    return (
+        <div className='fixed top-0 w-full h-[60px] bg-black border-b border-white/60 p-3 flex justify-between items-center z-50'>
+            <Link href='/'>
+                <h2 className='ml-7 font-bold text-xl'> PixelMind </h2>
+            </Link>
+            
+            <div className='mr-7'>
+                {initialLoading && status==="loading" ? <BiLoaderCircle className='animate-spin'/> : !session ? (
+                    <div className="__menu">
+                        <Button onClick={() => signIn("google")}>Login</Button>
+                    </div>) : (
+                    <Avatar>
+                        <AvatarImage src={session.user?.image || ""} />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                )}
+            </div>
+
+        </div>
+    )
+}
