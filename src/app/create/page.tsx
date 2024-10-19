8b09fb86a86a1fb8f4307809c29fe3e44e3921eb
aryaman <mohindruaryaman@gmail.com>
2024-10-20 02:20:30 +0530

    made site responsive

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,7 +1,6 @@
-"use client"
-import { Button } from '@/components/ui/button';
-import React, { useState } from 'react'
-import { z } from "zod"
+"use client";
+
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -9,12 +8,14 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
-import { useForm } from 'react-hook-form';
+import { Input } from "@/components/ui/input";
+import React, { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Input } from '@/components/ui/input';
-import Image from 'next/image';
-import { POST } from '../api/image/route';
-import { useToast } from '@/hooks/use-toast';
+import { useForm } from "react-hook-form";
+
+import { z } from "zod";
+import Image from "next/image";
+import { useToast } from "@/hooks/use-toast";
 
 const formSchema = z.object({
   prompt: z
@@ -22,13 +23,11 @@ const formSchema = z.object({
     .min(7, { message: "Prompt must be atleast 7 characters long!" }),
 });
 
-
-export default function page() {
-
+export default function Page() {
   const [outputImg, setOutputImg] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const {toast} = useToast()
+  const { toast } = useToast();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -42,21 +41,19 @@ export default function page() {
       setLoading(true);
       const response = await fetch("/api/image", {
         method: "POST",
-        body: JSON.stringify(values)
+        body: JSON.stringify(values),
       });
-      const data = await response.json();  //Extract the body from response
-      if(response.status === 200){
+      const data = await response.json();
+      if (response.status === 200) {
         setOutputImg(data.url);
-      }else{
-        console.log(data.error)
-        toast({variant:"destructive", description:data.error})
+      } else {
+        toast({ variant: "destructive", description: data.error });
       }
     } catch (error) {
-        console.error(error);
-    }finally{
+      console.error(error);
+    } finally {
       setLoading(false);
     }
-    
   }
 
   return (
@@ -64,11 +61,10 @@ export default function page() {
       <div className="w-full p-3">
         <h1 className="text-center font-bold text-white text-4xl">Create</h1>
         <p className="text-white/60 text-center">
-          And click profile pic to check your creations
+          Generate Stunning Images from Text for FREE
         </p>
       </div>
       <div className="flex w-full gap-3 h-[calc(100dvh-200px)] md:flex-row flex-col">
-
         <div className="__form flex-[2] h-full gap-2 flex justify-center items-start flex-col">
           <p className="text-center w-full lg:text-left text-sm text-white/80">
             Type you prompt below to create any image you can imagine!
@@ -102,14 +98,24 @@ export default function page() {
             </Form>
           </div>
         </div>
-
-        <div className='__output flex-[1] bg-white/5 rounded-lg relative overflow-hidden'>
-          {outputImg ? 
-          (<Image alt="output" className='w-full h-full object-contain' src={outputImg} width={300} height={300} />)
-          :<div className='w-full h-full flex justify-center items-center text-white/60 text-center p-3'>
-          Enter your prompt and hit Generate!</div>}
+        <div className="__output min-h-[300px] lg:min-h-full lg:h-full flex-[1] bg-white/5 rounded-lg relative overflow-hidden">
+          {outputImg ? (
+            <Image
+              alt="output"
+              className="w-full h-full object-contain"
+              src={outputImg}
+              width={300}
+              height={300}
+            />
+          ) : (
+            <>
+              <div className="w-full h-full flex justify-center items-center text-white/70 text-center p-3">
+                Enter your prompt and hit generate!
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
   );
-}
+} 
